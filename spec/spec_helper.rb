1ce9ff8ec135f755316ea8e3b3099668f2806193
Michal Cichra <michal@o2h.cz>
2014-08-02 19:38:29 +0200

    bring the gem to ruby 2.x era

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,8 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
-#require File.expand_path("../../config/environment", __FILE__)
-
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[File.dirname(__FILE__) + "/support/**/*.rb"].each {|f| require f}
 
+require 'norma43'
 RSpec.configure do |config|
   # == Mock Framework
   #
@@ -15,5 +12,4 @@ RSpec.configure do |config|
   # config.mock_with :flexmock
   # config.mock_with :rr
   config.mock_with :rspec
-    
 end
